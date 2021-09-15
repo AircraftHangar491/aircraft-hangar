f3690d731afa9a3392a7004520ef97cde6df3067
dkmacho <89963401+dkmacho@users.noreply.github.com>
2021-09-14 14:21:58 -1000

    Allowed a,b,c,d to be static

--- frontend/src/pages/Hangar/Hangar.js
+++ frontend/src/pages/Hangar/Hangar.js
@@ -6,10 +6,10 @@ class Hangar extends Component {
 
   render() {
 
-    const layout = [
+    const layout = [ 
       {i: 'a', x: 0, y: 0, w: 1, h: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+      {i: 'b', x: 0, y: 0, w: 4, h: 2},
+      {i: 'c', x: 0, y: 0, w: 1, h: 2},
     ];
 
     return (
@@ -22,8 +22,13 @@ class Hangar extends Component {
               rowHeight={30}
               width={1200}
               compactType="null"
+              isBounded={true}
             >
-              {layout.map((item) => <div key={item.i}>{item.i}</div>)}    
+              {layout.map((item) => <div key={item.i} data-grid={{x: item.x, y: item.y, w: item.w, h: item.h, static: true}}>{item.i}</div>)}   
+
+              <div key="d" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>d</div>
+              <div key="e" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>e</div>
+              <div key="f" data-grid={{x: 4, y: 0, w: 1, h: 2}}>f</div>
             </GridLayout>
         </main>
       </div>
