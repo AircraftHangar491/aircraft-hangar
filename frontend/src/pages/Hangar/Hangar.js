5c41f3702fe52a5872cf85fdcb8a48c73f74c902
dkmacho <89963401+dkmacho@users.noreply.github.com>
2021-09-14 15:01:10 -1000

    Update Hangar.js

--- frontend/src/pages/Hangar/Hangar.js
+++ frontend/src/pages/Hangar/Hangar.js
@@ -7,9 +7,9 @@ class Hangar extends Component {
   render() {
 
     const layout = [ 
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, bool: true},
-      {i: 'b', x: 0, y: 0, w: 4, h: 2},
-      {i: 'c', x: 0, y: 0, w: 1, h: 2, bool: true},
+      {i: 'a', x: 0, y: 0, w: 2, h: 2, bool: true},
+      {i: 'b', x: 0, y: 0, w: 10, h: 10},
+      {i: 'c', x: 0, y: 0, w: 1, h: 1, bool: true},
     ];
 
     return (
@@ -18,11 +18,14 @@ class Hangar extends Component {
           <GridLayout
               className="layout"
               layouts={layout}
-              cols={12}
-              rowHeight={30}
-              width={1200}
+              cols={40}
+              rowHeight={17.5}
+              width={1110}
               compactType="null"
-              isBounded={true}
+              isBounded={true} 
+              isResizable={false}
+              
+              
             >
               {layout.map((item) => <div key={item.i} data-grid={{x: item.x, y: item.y, w: item.w, h: item.h, static: item.bool}}>{item.i}</div>)}   
 
